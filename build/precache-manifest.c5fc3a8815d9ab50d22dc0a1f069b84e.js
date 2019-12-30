112362dda5d346acc049defd1c128a2658b91640
V <ramyavenkatesh182@gmail.com>
2019-12-30 10:18:48 +0530

    Adding monsters-rolodex project code

---
+++ build/precache-manifest.c5fc3a8815d9ab50d22dc0a1f069b84e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9826c7e9e187900da805340ea0900e21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "330c54741151fcd3a7c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "64ef9f9ea8a08ebcd8eb",
+    "url": "/static/js/2.c588ade7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.c588ade7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "330c54741151fcd3a7c8",
+    "url": "/static/js/main.058a8cac.chunk.js"
+  },
+  {
+    "revision": "249a37921e0703ab7925",
+    "url": "/static/js/runtime-main.04abcc89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
